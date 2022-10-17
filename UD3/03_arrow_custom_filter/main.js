b79993a5f957b7d3440af1e54326e6bc9f667a18
Ángel Gaudes <a25106@svalero.com>
2022-10-17 18:09:28 +0200

    Filtro arrow hecho

--- UD3/03_arrow_custom_filter/main.js
+++ UD3/03_arrow_custom_filter/main.js
@@ -1,15 +1,17 @@
 const assert = require('assert').strict;
 
-function select(array, condition) {
-}
+let select = (array, condition) => array.filter((i) => condition(i));
+let pairs = (value) => value % 2 === 0;
+let gt15 = (value) => value > 15;
+let lt10 = (value) => value < 10;
 
-let values = [1, 2, 3, 5, 7, 13, 17, 23, 29]
+let values = [1, 2, 3, 5, 7, 13, 17, 23, 29];
 
 // sólo pares
-assert.deepStrictEqual(select(values, pairs), [2])
+assert.deepStrictEqual(select(values, pairs), [2]);
 
 // mayores que 15
-assert.deepStrictEqual(select(values, gt15), [17, 23, 29])
+assert.deepStrictEqual(select(values, gt15), [17, 23, 29]);
 
 // menores de 10
-assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7])
+assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7]);
