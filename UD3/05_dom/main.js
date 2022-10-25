874757e0d2d4e4543629194ba98f003641c2aaf5
Ángel Gaudes <a25106@svalero.com>
2022-10-25 18:09:38 +0200

    Dom Exercises Done

---
+++ UD3/05_dom/main.js
@@ -0,0 +1,4 @@
+console.log(document.getElementsByTagName('div'));
+console.log(document.querySelectorAll('.buttons'));
+console.log(document.getElementsByClassName('buttons')[0]);
+console.log(document.querySelectorAll('div.buttons'));
