156689bfb82cf1a99a856180581e03c2ce19f113
Izarbe02 <zarbix02@gmail.com>
2024-10-30 19:59:53 +0100

    Ejercicios JS

---
+++ UD3/05_dom/main.js
@@ -0,0 +1,12 @@
+//document.querySelectorAll('div');
+//document.querySelectorAll('.buttons');
+//document.querySelector('.buttons');
+//document.querySelectorAll('div.buttons');
+let mostrarDivs = document.querySelectorAll('div')
+    for (let div of mostrarDivs){
+    console.log(div)
+}
+let mostrarBtn = document.querySelectorAll('.buttons')
+    for (let buttons of mostrarBtn){
+    console.log(buttons)
+}
